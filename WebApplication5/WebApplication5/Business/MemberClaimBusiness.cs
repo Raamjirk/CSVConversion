67bba7308038b292f5eb349051c66cdbd9df0d7b
Raamjirk <Raamji.05@gmail.com>
2021-04-05 12:56:54 -0700

    Update MemberClaimBusiness.cs

--- WebApplication5/WebApplication5/Business/MemberClaimBusiness.cs
+++ WebApplication5/WebApplication5/Business/MemberClaimBusiness.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebApplication5.Business
 {
+    [ScopedService]
     public class MemberClaimBusiness
     {
         private const string claimsPath = @"../WebApplication5/CSVs/Claim.csv";
